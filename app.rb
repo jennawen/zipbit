d0cbdc1d816102c9f353b10ced2fee2cb207a48b
Jenny Yee <jennyyee@Jennycat.local>
2013-10-12 12:46:08 -0700

    Getting an error ActiveRecord::AdapterNotSpecified

--- app.rb
+++ app.rb
@@ -1,7 +1,10 @@
 require 'sinatra'
 require 'sinatra/activerecord'
+require 'securerandom'
 require_relative 'models/listing'
 require_relative 'models/user'
+
+set :database, ENV['DATABASE_URL']
 
 begin 
   require 'dotenv'
@@ -9,14 +12,32 @@ begin
 rescue LoadError
 end
 
-set :database, ENV['DATABASE_URL']
+helpers do
+  def listings
+    @listings = Listing.all
+  end
+
+  def requested_listing
+    @requested_listing ||= Listing.find_by(secret_key: params[:secret_key])
+  end
+end
 
 get '/' do
-  @listings = Listing.all
   erb :index
 end
 
 post '/' do
-  Listing.create({title: params[:title], price: params[:price], description: params[:description]})
-  redirect '/'
+  @requested_listing = Listing.create({title: params[:title], price: params[:price], description: params[:description], secret_key: SecureRandom.hex(3)})
+  "Thank you for your submission. Please go to http://zipbit.herokuapp.com/views/#{requested_listing.secret_key} to edit your post."
 end
+
+
+get "/views/:secret_key" do
+  erb :editlisting
+end
+ 
+post '/confirmedit' do
+  requested_listing.update(params)
+  "Thank you!"
+end
+ 
