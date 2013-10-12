da52d6dcc2f650c0e3cd03d10ccdb4668c06f300
Jenny Yee <jennyyee@Jennycat.local>
2013-10-12 04:13:26 -0700

    created listing as a helper method

--- app.rb
+++ app.rb
@@ -12,11 +12,19 @@ end
 set :database, ENV['DATABASE_URL']
 
 get '/' do
-  @listings = Listing.all
+  # @listings = Listing.all
   erb :index
 end
 
 post '/' do
   Listing.create({title: params[:title], price: params[:price], description: params[:description]})
+  # @listings = Listing.all
   redirect '/'
+end
+
+helpers do
+
+  def listings
+    @listings = Listing.all
+  end
 end
